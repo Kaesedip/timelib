ef416555c05b0cf5d5c7464283331093d0a993c1
Thomas Siedler <Th.Siedler89@gmail.com>
2022-05-05 09:52:35 +0200

    formatting code

--- main.c
+++ main.c
@@ -1,45 +1,50 @@
-/**
- *
- *
- *
- **/
-
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 /**
  *
  *
  **/
-int tageProMonat [12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    int jahr, monat, tag;
-    int gesamtTage = 0;
-    int checkForRightDate = 1;
+int tageProMonat[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int jahr, monat, tag;
+int gesamtTage = 0;
+int checkForRightDate = 1;
 
-
-    //Funktion zum Überprüfen, ob das Jahr ein Schaltjahr ist.
-    int isSchaltjahr(int schaltJahr){
-        if (schaltJahr % 4 == 0){
-                if( schaltJahr % 100 == 0){
-                        if ( schaltJahr % 400 == 0){
-                            tageProMonat[1] = 29;
-                            return true;
-                        } else {
-                            return false;
-                        }
-
-                } else {
-                    tageProMonat[1] = 29;
-                    return true;
-                }
-            } else {
+// Funktion zum Ueberpruefen, ob das Jahr ein Schaltjahr ist.
+int isSchaltjahr(int schaltJahr)
+{
+    if (schaltJahr % 4 == 0)
+    {
+        if (schaltJahr % 100 == 0)
+        {
+            if (schaltJahr % 400 == 0)
+            {
+                tageProMonat[1] = 29;
+                return true;
+            }
+            else
+            {
                 return false;
             }
-
+        }
+        else
+        {
+            tageProMonat[1] = 29;
+            return true;
+        }
     }
+    else
+    {
+        return false;
+    }
+}
 
-    //Nutzer gibt Daten ein
-    while(checkForRightDate){
+// Nutzer gibt Daten ein
+int userInput()
+{
+    while (checkForRightDate)
+    {
 
         printf("\nGeben Sie das Jahr ein: ");
         scanf("%i", &jahr);
@@ -47,49 +52,63 @@ int tageProMonat [12] = {31,28,31,30,31,30,31,31,30,31,30,31};
         isSchaltjahr(jahr);
 
         printf("\nGeben Sie den Monat ein: ");
-        scanf("\n %i",&monat);
+        scanf("\n %i", &monat);
 
+        // Ueberpruefen ob die Eingaben korrekt sind
+        if (monat > 12)
+        {
+            printf("Ungueltiger Wert fuer den Monat.");
+            continue;
+        }
 
         printf("\nGeben Sie den Tag ein: ");
         scanf("\n %i", &tag);
 
-        //Ueberprüfen ob die Eingaben korrekt sind
-        if(monat > 12){
-            printf("Ungueltiger Wert fuer den Monat.");
-        }else if(tag > 31){
+        // Ueberpruefen ob die Eingaben korrekt sind
+        if (tag > 31)
+        {
             printf("Ungueltiger Wert fuer den Tag.");
-        } else if (isSchaltjahr(jahr) && tag > 29 && monat == 2){
-            printf("Ungueltiger Wert fuer den Tag.");
-        } else if(tag > 28 && monat == 2 && !isSchaltjahr(jahr)){
+
+        }
+        else if (isSchaltjahr(jahr) && tag > 29 && monat == 2)
+        {
             printf("Ungueltiger Wert fuer den Tag.");
         }
-        else{
+        else if (tag > 28 && monat == 2 && !isSchaltjahr(jahr))
+        {
+            printf("Ungueltiger Wert fuer den Tag.");
+        }
+        else
+        {
             checkForRightDate = 0;
         }
     }
+}
 
+//Berechnung der bisherigen Tage in diesem Jahr
+int day_of_the_year(int monat, int tag)
+{
 
-
-    int tagDesJahres(int monat, int tag){
-
-        if(monat == 1){
-            return tag;
-        }
-        else{
-
-            for (int i = 1 ; i< monat; i++){
-            gesamtTage += tageProMonat[i-1];
-        }
-         return gesamtTage += tag;
-        }
+    if (monat == 1)
+    {
+        return tag;
     }
+    else
+    {
 
-    printf("Heute ist der %i. Tag des Jahres. Datum %i.%i.%i",tagDesJahres(monat,tag),tag,monat,jahr);
-
+        for (int i = 1; i < monat; i++)
+        {
+            gesamtTage += tageProMonat[i - 1];
+        }
+        return gesamtTage += tag;
+    }
+}
 
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    userInput();
+    isSchaltjahr(jahr);
+    printf("Tag des Jahres: %i\n", day_of_the_year(monat, tag));
     return 0;
 }
